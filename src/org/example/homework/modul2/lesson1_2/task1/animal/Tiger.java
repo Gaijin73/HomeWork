a5d2c84e9d7b1277bb8f90bba9cd6c7d12206cd4
Maxim Leshin <gaijin731@mail.ru>
2024-01-16 01:01:20 +0300

    Дз 1 из модуля 2

--- src/org/example/homework/modul2/lesson1_2/task1/animal/Tiger.java
+++ src/org/example/homework/modul2/lesson1_2/task1/animal/Tiger.java
@@ -3,6 +3,6 @@ package org.example.homework.modul2.lesson1_2.task1.animal;
 public class Tiger extends Animal {
     @Override
     public void action() {
-        System.out.println("Лев рычит");
+        System.out.println("Тигр рычит");
     }
 }
